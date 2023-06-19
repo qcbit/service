01bdbc702cd37c89d954233a8c6aab5883493857
Lance <swdeveloper@skiff.com>
2023-06-19 18:51:16 -0500

    panic handling

--- app/services/sales-api/handlers/v1/testgrp/testgrp.go
+++ app/services/sales-api/handlers/v1/testgrp/testgrp.go
@@ -6,15 +6,16 @@ import (
 	"errors"
 	"math/rand"
 
-	// v1web "github.com/qcbit/services/business/web/v1"
+	v1web "github.com/qcbit/services/business/web/v1"
 	"github.com/qcbit/services/foundation/web"
 )
 
 // Status represents a test handler.
 func Status(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
 	if n := rand.Intn(100); n%2 == 0 {
-		// return v1web.NewRequestError(errors.New("trusted error"), http.StatusBadRequest)
-		return errors.New("non-trusted error")
+		return v1web.NewRequestError(errors.New("trusted error"), http.StatusBadRequest)
+		// return errors.New("non-trusted error")
+		// panic("oh no we panic")
 	}
 	status := struct {
 		Status string

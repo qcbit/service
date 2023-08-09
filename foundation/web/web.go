d320dfac3dd1445b68f1acac6e7af30f82cfdbde
TruongL <5490853+qcbit@users.noreply.github.com>
2023-08-08 20:53:34 -0500

    Added web framework

---
+++ foundation/web/web.go
@@ -0,0 +1,59 @@
+// Package web contains a small web framework for extension.
+package web
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/dimfeld/httptreemux/v5"
+	"github.com/google/uuid"
+)
+
+// A Handler is a type that handles an http request within our own little mini framework.
+type Handler func(ctx context.Context, w http.ResponseWriter, r *http.Request) error
+
+// App is the entrypoint into our application and what configures
+// our context object for each of our http handlers.
+// Feel free to add any configuration data/logic to this App struct.
+type App struct {
+	*httptreemux.ContextMux
+	shutdown chan os.Signal
+	mw       []Middleware
+}
+
+// NewApp creates an App value that handle a set of routes for the application.
+func NewApp(shutdown chan os.Signal, mw ...Middleware) *App {
+	return &App{
+		ContextMux: httptreemux.NewContextMux(),
+		shutdown:   shutdown,
+		mw:         mw,
+	}
+}
+
+// Handle sets a handler function for a given HTTP method and path pair to the application server mux.
+func (a *App) Handle(method string, path string, handler Handler, mw ...Middleware) {
+	handler = wrapMiddleware(mw, handler)
+	handler = wrapMiddleware(a.mw, handler)
+
+	h := func(w http.ResponseWriter, r *http.Request) {
+
+		v := Values{
+			TraceID: uuid.New().String(),
+			Now:     time.Now(),
+		}
+		ctx := context.WithValue(r.Context(), key, &v)
+
+		if err := handler(ctx, w, r); err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		// ADD CODE HERE: LOG
+	}
+
+	a.ContextMux.Handle(method, path, h)
+
+}

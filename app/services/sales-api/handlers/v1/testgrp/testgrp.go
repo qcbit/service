0f55aaaeec8275182990141e8fcee3feca80fb66
TruongL <5490853+qcbit@users.noreply.github.com>
2023-08-08 20:56:37 -0500

    Added middleware

--- app/services/sales-api/handlers/v1/testgrp/testgrp.go
+++ app/services/sales-api/handlers/v1/testgrp/testgrp.go
@@ -2,8 +2,9 @@ package testgrp
 
 import (
 	"context"
-	"encoding/json"
 	"net/http"
+
+	"github.com/qcbit/service/foundation/web"
 )
 
 // Test is our example route.
@@ -14,5 +15,5 @@ func Test(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
 		Status: "OK",
 	}
 
-	return json.NewEncoder(w).Encode(status)
+	return web.Respond(ctx, w, status, http.StatusOK)
 }

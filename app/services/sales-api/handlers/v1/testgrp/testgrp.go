d4ce42758bbc290cff454346836c8d8160d0165e
TruongL <5490853+qcbit@users.noreply.github.com>
2023-08-11 19:43:28 -0500

    Error handling

--- app/services/sales-api/handlers/v1/testgrp/testgrp.go
+++ app/services/sales-api/handlers/v1/testgrp/testgrp.go
@@ -2,13 +2,19 @@ package testgrp
 
 import (
 	"context"
+	"errors"
+	"math/rand"
 	"net/http"
 
+	v1 "github.com/qcbit/service/business/web/v1"
 	"github.com/qcbit/service/foundation/web"
 )
 
 // Test is our example route.
 func Test(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+	if n := rand.Intn(100); n%2 == 0 {
+		return v1.NewRequestError(errors.New("TRUSTED ERROR"), http.StatusBadRequest)
+	}
 	status := struct {
 		Status string
 	}{
